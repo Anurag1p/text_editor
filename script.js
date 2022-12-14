bffc4c3c1b557e6d888e256b210d02f8f0de3173
Anurag1p <anuragpal211@gmail.com>
2022-12-14 17:50:01 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+function redColor() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.color = "red";
+}
+function greenColor() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.color = "green";
+}
+function orangeColor() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.color = "orange";
+}
+function indianredColor() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.color = "purple";
+}
+
+function VioletColor() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.color = "Violet";
+}
+
+// for size ...........
+
+function sSize() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.fontSize = "small";
+}
+function mSize() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.fontSize = "medium";
+}
+
+function lSize() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.fontSize = "large";
+}
+
+//   for font family........
+
+function Family1() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.fontFamily = "verdana";
+}
+function Family2() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.fontFamily = "san-serif";
+}
+
+function Family3() {
+  const allname = document.getElementsByClassName("container");
+  allname[0].style.fontFamily = "Helvetica";
+}
+
+// font-type .........
+function fontType1() {
+    const allname = document.getElementsByClassName("container");
+    allname[0].style.fontWeight = "Bold";
+  }
+  function fontType2() {
+    const allname = document.getElementsByClassName("container");
+    allname[0].style.fontStyle = "italic";
+  }
+  
+  function fontType3() {
+    const allname = document.getElementsByClassName("container");
+    allname[0].style.textDecoration = "underline";
+  }
+  
+
+
+  function clearInput(){
+    window.location.reload()
+  }
